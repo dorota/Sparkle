a9b2d2131ceff7917c45c2b7cd1ac24c33a8898e
unknown <Dorota@.(none)>
2010-11-14 10:34:24 +0100

    gui refactored - some garbage deleted

--- SparkleMainProjectEclipse/Sparkle/src/Interface/MainWindow.java
+++ SparkleMainProjectEclipse/Sparkle/src/Interface/MainWindow.java
@@ -3,8 +3,6 @@ package Interface;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 
 import javax.media.j3d.Canvas3D;
 import javax.swing.JFrame;
@@ -33,47 +31,12 @@ class MainWindow
         int minWindowHeight = 300;
         _frame.setMinimumSize( new Dimension( minWindowWidth, minWindowHeight ) );
         _frame.setExtendedState( JFrame.MAXIMIZED_BOTH );
-        final int menuPanelWidth = 200;
-        _frame.addComponentListener( new ComponentListener()
-        {
-            public void componentResized( ComponentEvent e )
-            {
-                int frameWidth = _frame.getWidth();
-                int frameHeight = _frame.getHeight();
-                // _sceneCanvas.setSize( new Dimension( frameWidth -
-                // menuPanelWidth, frameHeight ) );
-                // _menuPanel.setSize( new Dimension( menuPanelWidth,
-                // frameHeight ) );
-                System.out.println( "how it should be" + _frame.getWidth() + " "
-                        + ( frameWidth - menuPanelWidth ) + " " + menuPanelWidth );
-                System.out.println( "how it is" + _frame.getWidth() + " " + _sceneCanvas.getWidth()
-                        + " " + _menuPanel.getWidth() );
-            }
-
-            public void componentMoved( ComponentEvent e )
-            {
-            }
-
-            public void componentHidden( ComponentEvent e )
-            {
-            }
-
-            public void componentShown( ComponentEvent e )
-            {
-            }
-        } );
         _sceneCanvas.setBackground( Color.black );
-        int frameWidth = _frame.getWidth();
-        int frameHeight = _frame.getHeight();
         BorderLayout layout = new BorderLayout();
         _frame.setLayout( layout );
         _frame.setResizable( true );
-        // _menuPanel.setSize( new Dimension( menuPanelWidth, frameHeight ) );
         _frame.add( _menuPanel, BorderLayout.EAST );
-        // _sceneCanvas.setSize( new Dimension( frameWidth - menuPanelWidth,
-        // frameHeight ) );
         _frame.add( _sceneCanvas, BorderLayout.CENTER );
-        System.out.println( _menuPanel.getWidth() + " " + _menuPanel.getHeight() );
         _frame.pack();
         _frame.setVisible( true );
     }

5cad28a7496c491801085cc7aa232ac60858c44c
unknown <Dorota@.(none)>
2010-11-14 10:30:40 +0100

    simple gui done

--- SparkleMainProjectEclipse/Sparkle/src/Interface/MainWindow.java
+++ SparkleMainProjectEclipse/Sparkle/src/Interface/MainWindow.java
@@ -1,47 +1,53 @@
 package Interface;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.ComponentOrientation;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 
 import javax.media.j3d.Canvas3D;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 import com.sun.j3d.utils.universe.SimpleUniverse;
 
 class MainWindow
 {
-    JFrame _frame;
-    Canvas3D _sceneCanvas;
-    JPanel _menuPanel;
-    Scene3D _scene;
+    private JFrame _frame;
+    private Canvas3D _sceneCanvas;
+    private MenuPanel _menuPanel;
+    private Scene3D _scene;
 
     public MainWindow()
     {
         _frame = new JFrame();
         _sceneCanvas = new Canvas3D( SimpleUniverse.getPreferredConfiguration() );
-        _menuPanel = new JPanel();
+        _menuPanel = new MenuPanel();
         _scene = new Scene3D( _sceneCanvas );
     }
 
-    public void init()
+    public void initWindow()
     {
         _frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-        _frame.setMinimumSize( new Dimension( 1000, 300 ) );
+        int minWindowWidth = 1000;
+        int minWindowHeight = 300;
+        _frame.setMinimumSize( new Dimension( minWindowWidth, minWindowHeight ) );
         _frame.setExtendedState( JFrame.MAXIMIZED_BOTH );
+        final int menuPanelWidth = 200;
         _frame.addComponentListener( new ComponentListener()
         {
-            @SuppressWarnings( "deprecation" )
             public void componentResized( ComponentEvent e )
             {
                 int frameWidth = _frame.getWidth();
                 int frameHeight = _frame.getHeight();
-                _sceneCanvas.resize( new Dimension( (int)( 0.75 * frameWidth ), frameHeight ) );
-                _menuPanel.resize( new Dimension( (int)( 0.25 * frameWidth ), frameHeight ) );
+                // _sceneCanvas.setSize( new Dimension( frameWidth -
+                // menuPanelWidth, frameHeight ) );
+                // _menuPanel.setSize( new Dimension( menuPanelWidth,
+                // frameHeight ) );
+                System.out.println( "how it should be" + _frame.getWidth() + " "
+                        + ( frameWidth - menuPanelWidth ) + " " + menuPanelWidth );
+                System.out.println( "how it is" + _frame.getWidth() + " " + _sceneCanvas.getWidth()
+                        + " " + _menuPanel.getWidth() );
             }
 
             public void componentMoved( ComponentEvent e )
@@ -59,13 +65,15 @@ class MainWindow
         _sceneCanvas.setBackground( Color.black );
         int frameWidth = _frame.getWidth();
         int frameHeight = _frame.getHeight();
-        FlowLayout layout = new FlowLayout( FlowLayout.LEADING );
+        BorderLayout layout = new BorderLayout();
         _frame.setLayout( layout );
-        _frame.setComponentOrientation( ComponentOrientation.LEFT_TO_RIGHT );
         _frame.setResizable( true );
-        _sceneCanvas.setSize( new Dimension( (int)( 0.75 * frameWidth ), frameHeight ) );
-        _menuPanel.setSize( new Dimension( (int)( 0.25 * frameWidth ), frameHeight ) );
-        _frame.getContentPane().add( _sceneCanvas );
+        // _menuPanel.setSize( new Dimension( menuPanelWidth, frameHeight ) );
+        _frame.add( _menuPanel, BorderLayout.EAST );
+        // _sceneCanvas.setSize( new Dimension( frameWidth - menuPanelWidth,
+        // frameHeight ) );
+        _frame.add( _sceneCanvas, BorderLayout.CENTER );
+        System.out.println( _menuPanel.getWidth() + " " + _menuPanel.getHeight() );
         _frame.pack();
         _frame.setVisible( true );
     }
@@ -73,6 +81,6 @@ class MainWindow
     public static void main( String[] args )
     {
         MainWindow mainWindow = new MainWindow();
-        mainWindow.init();
+        mainWindow.initWindow();
     }
 }

03f34714edb0a05207b64da81c9259bc4d22dadb
unknown <Dorota@.(none)>
2010-11-28 08:45:54 +0100

    WorldSceneMediator tested

---
+++ SparkleMainProjectEclipse/Sparkle/Tests/Helpers/WorldSceneMediatorTest.java
@@ -0,0 +1,46 @@
+package Helpers;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Logic.World;
+
+public class WorldSceneMediatorTest
+{
+    private int _realWorldSize = World.getMAX_LENGTH();
+
+    @Before
+    public void testPrep()
+    {
+        World.setMAX_LENGTH( 4 );
+    }
+
+    public void testCleanUp()
+    {
+        World.setMAX_LENGTH( _realWorldSize );
+    }
+
+    @Test
+    public void testChangeWorldIndexToSceneIndex()
+    {
+        assertEquals( "2 od lewej, 1 piêtro, przedostatni rz¹d", WorldSceneMediator
+                .changeWorldIndexToSceneIndex( 1, 0, 1 ), 5 );
+        assertEquals( 12, WorldSceneMediator.changeWorldIndexToSceneIndex( 0, 0, 3 ) );
+    }
+
+    @Test
+    public void testChangeSceneIndexToWorldIndex()
+    {
+        WorldSceneMediator.WorldIndex indexResult = new WorldSceneMediator.WorldIndex();
+        indexResult.x = 0;
+        indexResult.y = 0;
+        indexResult.z = 3;
+        assertEquals( indexResult, WorldSceneMediator.changeSceneIndexToWorldIndex( 12 ) );
+        indexResult.x = 1;
+        indexResult.y = 0;
+        indexResult.z = 1;
+        assertEquals( indexResult, WorldSceneMediator.changeSceneIndexToWorldIndex( 5 ) );
+    }
+}
--- SparkleMainProjectEclipse/Sparkle/src/Helpers/WorldSceneMediator.java
+++ SparkleMainProjectEclipse/Sparkle/src/Helpers/WorldSceneMediator.java
@@ -7,7 +7,8 @@ public class WorldSceneMediator
 {
     public static int changeWorldIndexToSceneIndex( int worldX, int worldY, int worldZ )
     {
-        return worldY * ( worldX * worldZ ) + worldZ * worldX + worldX;
+        return worldY * ( World.getMAX_LENGTH() * World.getMAX_LENGTH() ) + worldZ
+                * World.getMAX_LENGTH() + worldX;
     }
 
     public static WorldIndex changeSceneIndexToWorldIndex( int i )
@@ -25,5 +26,26 @@ public class WorldSceneMediator
         public int x;
         public int y;
         public int z;
+
+        @Override
+        public String toString()
+        {
+            return "[ " + x + " " + y + " " + z + " ]";
+        }
+
+        @Override
+        public boolean equals( Object o )
+        {
+            if( o == this )
+            {
+                return true;
+            }
+            if( !( o instanceof WorldIndex ) )
+            {
+                return false;
+            }
+            WorldIndex wi = (WorldIndex)o;
+            return wi.x == this.x && wi.y == this.y && wi.z == this.z;
+        }
     };
 }

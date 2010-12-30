171d0cae1c19e6c23c2d05027dab6a1688947dcf
Jacek Zlydach <temporal.pl@gmail.com>
2010-12-30 23:04:52 +0100

    Profiler now displays statistics instead of full data set.

--- SparkleMainProjectEclipse/Sparkle/src/Helpers/ProfileSample.java
+++ SparkleMainProjectEclipse/Sparkle/src/Helpers/ProfileSample.java
@@ -5,7 +5,10 @@
 package Helpers;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static Helpers.Statistics.*;
 
 /**
  *
@@ -24,49 +27,65 @@ public class ProfileSample {
 
     public ProfileSample(String sampleName)
     {
-	name = sampleName;
-	allSamples.add(this);
+        name = sampleName;
+        allSamples.add(this); //AD. warning - yes, I know.
     }
 
     public ProfileSample start()
     {
-	startTime = System.nanoTime();
-	return this;
+        startTime = System.nanoTime();
+        return this;
     }
 
     public ProfileSample stop()
     {
-	assert startTime != SAMPLE_NOT_INITIALIZED : "Hey, you've stopped the profiling without starting!";
+        assert startTime != SAMPLE_NOT_INITIALIZED : "Hey, you've stopped the profiling without starting!";
 
-	//add delta time to sample log
-	entries.add(System.nanoTime() - startTime);
+        //add delta time to sample log
+        entries.add(System.nanoTime() - startTime);
 
-	//reset counter
-	startTime = SAMPLE_NOT_INITIALIZED;
-	return this;
+        //reset counter
+        startTime = SAMPLE_NOT_INITIALIZED;
+        return this;
     }
 
     public void report()
     {
-	System.out.println("ENTRIES FOR SAMPLE " + name);
-	for(Long entry : entries)
-	{
-	    System.out.print(" " + entry);
-	}
-	System.out.println("");
+        System.out.println("ENTRIES FOR SAMPLE " + name);
+        for(Long entry : entries)
+        {
+            System.out.print(" " + entry);
+        }
+        System.out.println("");
     }
 
     public void reportStatistics()
     {
-	//TODO to be implemented when I figure out how one sorts lists in Java...
+	List<Long> entriesCopy = new ArrayList<Long>(entries);
+    
+        Collections.sort(entriesCopy);
+
+        long min = entriesCopy.get(0);
+        long max = entriesCopy.get(entriesCopy.size()-1);
+
+	//FIXME does anybody know how "kwartyle" are called in english?...
+        long kwartylDolny = computeKwartylDolny(entriesCopy);
+        long kwartylGorny = computeKwartylGorny(entriesCopy);
+        long median = computeMedian(entriesCopy);
+        System.out.println("STATS FOR SAMPLE " + name);
+        System.out.println("Min: " + min + "; Max " + max + "; Rozstep: " + (max-min));
+        System.out.println("Kwartyl dolny: " + kwartylDolny + "; Mediana: " + median + "; Kwartyl gorny: " + kwartylGorny + "; Rozstep miedzykwartylowy: " + (kwartylGorny - kwartylDolny) );
+	System.out.println(""); //empty line
     }
 
     public static void reportAll()
     {
-	for(ProfileSample sample : allSamples)
-	{
-	    sample.report();
-	}
+        for(ProfileSample sample : allSamples)
+        {
+            //sample.report(); <-- uncomment if you absolutely, positively have to look at all the data.
+	    sample.reportStatistics();
+        }
     }
-
 }
+
+
